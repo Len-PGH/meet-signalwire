dc833c00fdbb0cf4314905c2beb03de14e46f96f
Len <Len-PGH@users.noreply.github.com>
2021-10-22 10:16:11 -0400

    Create app_languages.php

---
+++ app/meet_signalwire/app_languages.php
@@ -0,0 +1,7 @@
+<?php
+
+$text['label-meet-signalwire-participant-token']['en-us'] = "Meet-Signalwire participant token";
+$text['label-meet_signalwire_moderator_token']['en-us'] = "Meet-Signalwire moderator token";
+$text['label-meet-signalwire-room']['en-us'] = "Meet-Signalwire Room Name";
+
+?>
