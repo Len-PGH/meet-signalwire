10cb49232d7e0b2e5e49fee09d995d0e07111d5b
Len <Len-PGH@users.noreply.github.com>
2021-10-22 10:24:03 -0400

    Update app_menu.php

--- app/meet_signalwire/app_menu.php
+++ app/meet_signalwire/app_menu.php
@@ -1,13 +1,15 @@
 <?php
 
-	$apps[$x]['menu'][0]['title']['en-us'] = 'Jitsi-Meet';
-	$apps[$x]['menu'][0]['uuid'] = 'aa36de5d-8fc1-4cee-9319-b64efd01a3f5';
+	$apps[$x]['menu'][0]['title']['en-us'] = 'Meet Signalwire';
+	$apps[$x]['menu'][0]['uuid'] = '09516a28-394d-408a-ac20-49ab751cd1cc';
 	$apps[$x]['menu'][0]['parent_uuid'] = 'cd544c95-921a-4386-94d9-81913bd8e949';
 	$apps[$x]['menu'][0]['category'] = 'internal';
-	$apps[$x]['menu'][0]['path'] = '/app/jitsimeet/jitsimeet.php';
+	$apps[$x]['menu'][0]['path'] = '/app/meet_signalwire/meet_signalwire.php';
 	$apps[$x]['menu'][0]['icon'] = "fa-yin-yang";
-	$apps[$x]['menu'][0]['order'] = "50";
+	$apps[$x]['menu'][0]['order'] = "51";
 	$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 	$apps[$x]['menu'][0]['groups'][] = 'admin';
+	$apps[$x]['menu'][0]['groups'][] = 'user';
+	
 
 ?>
